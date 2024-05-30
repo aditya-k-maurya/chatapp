df45481b814129154b3bfc1ba68f561dbb8ae067
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-05-30 09:15:12 +0530

    initial commit

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN,
+		credentials: true,
+	})
+);
+
+// middleware to get json data and we can also set the limit
+app.use(express.json({ limit: "16kb" }));
+
+// we can also get data from the url so to handle than we use middleware name urlencoded
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
